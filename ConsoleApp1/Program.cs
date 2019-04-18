6f6556362292546ae88e0c1f82aaa2fd08e993b1
Adam Holt <adam@hangupyourheadphones.com>
2019-04-18 13:20:22 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using WeatherConsole;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            var city = "New York, NY";
+            var client = new OpenWeatherMapClient();
+            Console.WriteLine($"Fetching weather for {city}");
+            var weather = await client.GetCurrentWeatherByCity(city);
+            if (weather == null)
+            {
+                Console.WriteLine("Failed to fetch weather information.");
+                return;
+            }
+            Console.WriteLine($"\nTemp: {weather.Main?.Temperature}");
+            Console.WriteLine($"Low: {weather.Main?.MinTemperature}");
+            Console.WriteLine($"High: {weather.Main?.MaxTemperature}");
+            Console.WriteLine($"Humidity: {weather.Main?.Humidity}%");
+            Console.WriteLine($"Condition: {weather.FirstCondition?.Description}");
+        }
+    }
+}
